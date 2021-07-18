b3b9dff2077d9b8305e5334b230f52ce217d4caf
Almog Futterman <almogtc5@gmail.com>
2021-07-18 23:20:47 +0300

    fixed static folder?

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+const path = require('path')
 require("./dbconnection")
 
-app.use('/', express.static("build"))
+app.use('/', express.static(path.join(__dirname, "build")))
 
 require("dotenv").config()
 const port = process.env.PORT
